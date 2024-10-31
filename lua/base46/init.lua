20d69eb349928f1626bdc1c3b6746e1e71cc9dfb
Chris Nguyen <chris.dungnguyen@gmail.com>
2024-10-31 12:38:29 +0700

    fix: checking themes

--- lua/base46/init.lua
+++ lua/base46/init.lua
@@ -112,8 +112,9 @@ M.load_all_highlights = function()
 end
 
 M.override_theme = function(default_theme, theme_name)
-	local changed_themes = {}
-	return M.merge_tb(default_theme, changed_themes.all or {}, changed_themes[theme_name] or {})
+--	local changed_themes = {}
+--	return M.merge_tb(default_theme, changed_themes.all or {}, changed_themes[theme_name] or {})
+	return default_theme
 end
 
 return M
